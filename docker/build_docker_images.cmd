087ecba2fbaccf6bd051ed41aa2c9777e76127c2
hesong <hesong@uxsino.com>
2021-08-05 16:11:09 +0800

    添加uxdbjdbc到spark jar库；修改部分已失效的连接

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,3 +1,4 @@
-docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.2 .
-docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.2 .
-docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
+docker build -f Dockerfile_base -t spark_base:1.0.0 .
+docker build -f Dockerfile_master -t spark_master:2.4.5 .
+docker build -f Dockerfile_worker -t spark_worker:2.4.5 .
+docker build -f Dockerfile_submit -t spark_submit:2.4.5 .
